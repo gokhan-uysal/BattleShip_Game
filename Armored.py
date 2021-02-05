a20dbb60f18eef056b9ce191c8fd114d6e22353d
Gökhan Uysal <guysal20@ku.edu.tr>
2021-02-05 16:04:55 +0300

    Updated

--- Armored.py
+++ Armored.py
@@ -36,7 +36,7 @@ class Armored():
             while (i == 0):
                 bulletType = str(input(f"Select the bullet type {self.name}:\nAP(Armor-Piercing) ,HP(High-Spreader)"))
                 if bulletType.upper() == "AP":
-                    hit = 300 * (self.numberOfCanons / (self.numberOfCanons+breaked))
+                    hit = 400 * (self.numberOfCanons / (self.numberOfCanons+breaked))
                     percent = random.randrange(1, 41) / 100
                     hp, armor = hit * percent, hit * (1 - percent)
                     i += 1
@@ -48,7 +48,7 @@ class Armored():
 
 
                 elif bulletType.upper() == "HP":
-                    hp = 325 * (self.numberOfCanons / (self.numberOfCanons+breaked))
+                    hp = 425 * (self.numberOfCanons / (self.numberOfCanons+breaked))
                     i += 1
                     print(
                         f"Firing HP shells with {self.numberOfCanons} out of {self.numberOfCanons+breaked} canons Captain {self.name}.\nHopping to deal {int(hp)} damage to the enemy ship!!")
